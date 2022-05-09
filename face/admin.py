544acbb6fd325f5bfac82075304df4c032331b0f
Naman Srivastava <srivastava.naman511@gmail.com>
2022-05-09 18:03:45 +0530

    added export function

--- face/admin.py
+++ face/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+import csv
+from django.http import HttpResponse
 from .models import *
 # Register your models here.
 
@@ -8,6 +10,36 @@ excluding_list_display = ('created_at','updated_at','is_active','deleted_at')
 excluding_fields = ('created_at','updated_at','is_active','deleted_at', 'paid_at')
 
 models = apps.get_app_config('face').get_models()
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+class TimingsAdmin(admin.ModelAdmin, ExportCsvMixin):
+    fields = ['name', 'time']
+    list_display = ['name', 'time']
+    actions = ["export_as_csv"]
+
+    # def export_as_csv(self, request, queryset):
+    #     pass
+
+    # export_as_csv.short_description = "Export Selected"
+
+admin.site.register(Timings, TimingsAdmin)
 
 for model in models:
     # continue
